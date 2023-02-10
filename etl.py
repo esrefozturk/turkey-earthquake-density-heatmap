74165a0f3af4dc3255136ce2e4177a9331b54817
Esref Ozturk <esrefozturk93@gmail.com>
2023-02-11 00:48:56 +0100

    initial project

---
+++ etl.py
@@ -0,0 +1,33 @@
+import plotly.express as px
+import requests
+from pandas import read_xml, DataFrame, concat
+
+url = 'http://udim.koeri.boun.edu.tr/zeqmap/xmlt/{month}.xml'
+
+years = list(range(2003, 2023))
+months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+df = DataFrame()
+
+for year in years:
+    for month in months:
+        t = requests.get(url.format(month=f'{year}{month}')).text
+
+        df_partial = read_xml(t)
+
+        df = concat([df, df_partial])
+
+for month in ['01', '02']:
+    t = requests.get(url.format(month=f'2023{month}')).text
+
+    df_partial = read_xml(t)
+
+    df = concat([df, df_partial])
+
+df.to_csv('data.csv')
+
+fig = px.density_mapbox(df, lat='lat', lon='lng', z='mag', radius=2,
+                        center=dict(lat=0, lon=180), zoom=0,
+                        mapbox_style="stamen-terrain")
+
+open('graph.html', 'w').write(fig.to_html())
